b6c5a7499a5d05ee921ea4a84c0d5172c7f72e48
Matthew <Matthew@greyhoundit.co.uk>
2022-12-01 20:04:09 +0000

    add benchmarking and a test to see if sort is faster then just pushing highest value

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,6 +1,7 @@
 require 'pry'
-class Day1
+require './lib/timing.rb'
 
+class Day1
   attr_reader :elves
   def initialize
     file_path = File.expand_path('input.csv', __dir__)
@@ -25,7 +26,16 @@ class Day1
     calories.sort.last(3).sum
   end
 
+  def part_one_test
+    calories = []
+    elves.map do |elf|
+      calories << elf.split("\n").map(&:to_i).sum
+    end
+    calories.sort.last
+  end
+
 end
 
-puts Day1.new.part_one
-puts Day1.new.part_two
+puts "part 1 #{Day1.new.part_one} time = #{Timing.time do Day1.new.part_one end}"
+puts "part 1 test #{Day1.new.part_one_test} time = #{Timing.time do Day1.new.part_one_test end}"
+puts "part 2 #{Day1.new.part_two} time = #{Timing.time do Day1.new.part_two end}"
